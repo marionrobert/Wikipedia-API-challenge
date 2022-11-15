6e1b57dd0f0e7d233b36ece14f96e83fc1ceac69
Marion <mrobert148@gmail.com>
2022-11-15 18:22:24 +0100

    create and connect WikiDB

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
+// connect app.js to the DB and create it if doesn't exist
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+// create the schemas
+const articlesSchema = {
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}
+
+// create the model based on the schema
+const Article = mongoose.model("Article", articlesSchema)
